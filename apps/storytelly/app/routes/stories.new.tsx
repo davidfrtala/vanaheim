97f39fc9d211e48601dc8d0fff603f9bf4d9ac7b
David Frtala <david.frtala@gmail.com>
2023-11-11 00:17:32 +0100

    feat(stories/new): wrap the editor with a three column layout

--- apps/storytelly/app/routes/stories.new.tsx
+++ apps/storytelly/app/routes/stories.new.tsx
@@ -11,7 +11,19 @@ export const links: LinksFunction = () => [
 export default function Editor() {
   return (
     <Suspense fallback={<p>Loading lazy chunk...</p>}>
-      <LazyComponent />
+      <div className="flex flex-wrap h-screen-minus-navbar -m-8 -mt-6">
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/6 xl:w-1/6">
+          <div className="h-full p-4 ">Left Sidebar</div>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/2 border-l border-r">
+          <div className="h-full p-4">
+            <LazyComponent />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+          <div className="h-full p-4">Right Sidebar</div>
+        </div>
+      </div>
     </Suspense>
   );
 }
