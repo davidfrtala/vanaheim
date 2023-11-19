ae83da20638a7364644e706c0a7c21afb6f3bba8
David Frtala <david.frtala@gmail.com>
2023-11-19 22:29:34 +0100

    refactor: replace chapters nodes with single container nodes

--- apps/storytelly/app/components/narative/chapters.ts
+++ apps/storytelly/app/components/narative/chapters.ts
@@ -1,290 +1,403 @@
 import { toEditorState } from './utils';
-export const Beginning = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '0) Beginning',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'Once upon a time',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
 
-export const Departure = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '1) Departure',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'Im going on an adventure',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const Beginning = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Beginning',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'Once upon a time',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
-export const Stay = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '2) Stay at home',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'Nah... not intersted. Gonna smoke some weed instead',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const Departure = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Departure',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'Im going on an adventure',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
-export const YoloSolo = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '5) Yolo Solo',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'Come get some!',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const Stay = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Stay at home',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'Nah... not intersted. Gonna smoke some weed instead',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
-export const GetSomeHelp = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '4) Get some help',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'Gandalf! Get over here!',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const YoloSolo = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Yolo Solo',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'Come get some!',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
-export const GotLost = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '3) Got lost',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: 'This place I dont recognize',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const GetSomeHelp = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Get some help',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'Gandalf! Get over here!',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
-export const Ending = [
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: '6) Bittersweet ending',
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'heading',
-    version: 1,
-    tag: 'h1',
-  },
-  {
-    children: [
-      {
-        detail: 0,
-        format: 0,
-        mode: 'normal',
-        style: '',
-        text: `I'm back, but I'm not the same person I was before`,
-        type: 'text',
-        version: 1,
-      },
-    ],
-    direction: 'ltr',
-    format: '',
-    indent: 0,
-    type: 'paragraph',
-    version: 1,
-  },
-];
+export const GotLost = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Got lost',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: 'This place I dont recognize',
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
+
+export const Ending = {
+  children: [
+    {
+      children: [
+        {
+          detail: 0,
+          format: 0,
+          mode: 'normal',
+          style: '',
+          text: 'Bittersweet ending',
+          type: 'text',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-title',
+      version: 1,
+    },
+    {
+      children: [
+        {
+          children: [
+            {
+              detail: 0,
+              format: 0,
+              mode: 'normal',
+              style: '',
+              text: `I'm back, but I'm not the same person I was before`,
+              type: 'text',
+              version: 1,
+            },
+          ],
+          direction: 'ltr',
+          format: '',
+          indent: 0,
+          type: 'paragraph',
+          version: 1,
+        },
+      ],
+      direction: 'ltr',
+      format: '',
+      indent: 0,
+      type: 'collapsible-content',
+      version: 1,
+    },
+  ],
+  direction: 'ltr',
+  format: '',
+  indent: 0,
+  type: 'collapsible-container',
+  version: 1,
+  open: true,
+};
 
 export const editorState = toEditorState([
-  ...Beginning,
-  ...Departure,
-  ...Stay,
-  ...YoloSolo,
-  ...GetSomeHelp,
-  ...GotLost,
-  ...Ending,
+  Beginning,
+  Departure,
+  Stay,
+  YoloSolo,
+  GetSomeHelp,
+  GotLost,
+  Ending,
 ]);
--- apps/storytelly/app/components/narative/useChapterUpdate.ts
+++ apps/storytelly/app/components/narative/useChapterUpdate.ts
@@ -14,7 +14,7 @@ export function useChapterUpdate() {
         .chapter;
 
       if (chapterContent) {
-        return acc.concat(...chapterContent);
+        return acc.concat(chapterContent);
       }
       return acc;
     }, []);
